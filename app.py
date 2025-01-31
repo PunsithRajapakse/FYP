18a0c82973960e2c45276ec5d847ddfad3729650
PunsithRajapakse <109160341+PunsithRajapakse@users.noreply.github.com>
2025-01-31 13:39:56 +0530

    new

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ def calculate_calories(food_type, radius_cm):
     calories = weight * calories_per_g
     return weight, calories
 
+
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
